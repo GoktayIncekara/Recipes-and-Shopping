266405c7ecc14691ae57ee94e9053b183a1bf95a
Göktay İncekara <goktayincekara@gmail.com>
2023-03-12 14:46:06 +0300

    Add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FirstProject';
-  loadedFeature= "recipe";
-
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
-  }
 }
