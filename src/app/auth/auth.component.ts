139a52397486a7c52bf862f34f02fb2e8ca91890
Göktay İncekara <goktayincekara@gmail.com>
2023-03-22 15:26:46 +0300

    Signup and error handling is added

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from './auth.service';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
+})
+export class AuthComponent implements OnInit {
+  isLoginMode = true;
+  isLoading = false;
+  error: string = "";
+  authForm!: FormGroup;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authForm = new FormGroup({
+      "email": new FormControl(null, [Validators.required, Validators.email]),
+      "password": new FormControl(null, [Validators.required, Validators.minLength(6)])
+    })
+  }
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit() {
+    if (!this.authForm.valid) {
+      return;
+    }
+    const email = this.authForm.value.email;
+    const password = this.authForm.value.password;
+
+    this.isLoading = true;
+    if (this.isLoginMode) {
+
+    } else {
+      this.authService.signup(email,password).subscribe(resData => {
+        console.log(resData);
+        this.isLoading = false;
+      }, errorRes => {
+        this.error = errorRes.message;
+        this.isLoading = false;
+      });
+    }
+
+    this.authForm.reset();
+  }
+}
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
+
+interface AuthResponseData {
+    kind: string;
+    idToken: string;
+    email: string;
+    refreshToken: string;
+    expiresIn: string;
+    localId: string;
+}
+
+@Injectable({providedIn: "root"})
+export class AuthService {
+    constructor(private http: HttpClient) {}
+
+    signup(email: string, password: string) {
+        return this.http.post<AuthResponseData>("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAttd-yP5aYiGx_Fd-9IMJT_ofIfMiwUpo",
+        {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }).pipe(catchError(errorRes => {
+            let errorMessage = "An unknown error occurred!";
+            switch(errorRes.error?.error?.message) {
+                case("EMAIL_EXISTS"):
+                    errorMessage = "This email is already exists";
+            }
+            return throwError(()  => new Error(errorMessage));
+        })
+            
+        );
+    }
+}
