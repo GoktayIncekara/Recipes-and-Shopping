3c406e7dcc5d024bea1dd5d713efb3bcce709c47
Göktay İncekara <goktayincekara@gmail.com>
2023-03-26 22:22:55 +0300

    environment variables are added

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  firebaseAPIKey: 'AIzaSyAttd-yP5aYiGx_Fd-9IMJT_ofIfMiwUpo',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  firebaseAPIKey: 'AIzaSyAttd-yP5aYiGx_Fd-9IMJT_ofIfMiwUpo',
+};
